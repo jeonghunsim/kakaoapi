5b8e14bf0ecb51a338d6aa1470b39a21c10c29e4
Jeong-hun <Jeong-hun@junghunsim>
2017-08-09 15:13:31 +0900

    Message Listen 부분 및 대화 해석 으로 넘기는 부분

--- init_service.js
+++ init_service.js
@@ -3,6 +3,9 @@
   * 17.08.03 
 */
 
+//FILE UPLOAD
+var language_understanding = require('./lu_processing.js');
+
 //NPM
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -19,5 +22,21 @@ var app = express();
 	app.use(express.static('public'));
 
 app.lissten(app.get('port'), function(){
+	
+});
+
+app.get('/keyboard',function(req,rew){
+	
+	
+});
+
+app.get('/message', function(req, res){ //message input
+	
+	var data = req.body;
+	var t_user_key = data.user_key; //" 착신 가입자(메시지 발신자) user key "
+	var type = data.type;           //" text, photo "
+	var content = data.content;     //" 자동 응답 명령어의 메시지 텍스트 혹은 미디어 파일 URI"
+	
+	// Message 처리에 대한 내용을 이후에 작성 
 	
 });
---
+++ lu_processing.js
@@ -0,0 +1,15 @@
+/**
+ * Language Processing Part
+ * 
+ */
+
+var contents_input = function(t_user,type,content){
+
+	// t_user : 착신자
+	// content : content 유형
+	// type : text or photo
+		
+}
+
+
+
