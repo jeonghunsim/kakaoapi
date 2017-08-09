cb877928ce39ad5b44e13aade17b0882f647a789
Jeong-hun <Jeong-hun@junghunsim>
2017-08-09 14:40:53 +0900

    add json

---
+++ json/json_template.js
@@ -0,0 +1,17 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+var Home_Keyboard_API = function(type_num,button){
+
+	var template;
+		// type = 1 button , type 2 = text
+	var type
+	switch(type_num){
+	
+	case 1 : break;
+	case 2 : break;
+	}
+	
+	return template;
+}
