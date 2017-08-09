e3d585fc80869dfcd3f0a116e42f76c70019b73d
Jeong-hun <Jeong-hun@junghunsim>
2017-08-09 16:51:59 +0900

    test update

--- lu_processing.js
+++ lu_processing.js
@@ -3,17 +3,26 @@
  * 
  */
 
+let init_message_text = {};
+init_message_text.Type = () => {
+         return {
+          type: 'text'          
+         }
+};
 
-
-exports.contents_input = function(t_user,type,content){
+exports.contents_input = function(res,t_user,type,content){
 
 	// t_user : 착신자
 	// content : content 유형
 	// type : text or photo
 	
-	//text 형태 활성화
-	
-			
+	//text 형태 활성화	
+	res.set({
+		'content-type' : 'application/json'
+		}).send(JSON.stringify(init_message_text.Type()));
+		//.send(JSON.stringify(init_message_button.buttonsType())); --> button Key Type
+		
+		//log.L('Text_Mode Activate...',filename,59);		
 }
 
 
